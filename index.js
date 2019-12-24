c7b49792c6ea03116d8814a28b15a055ed0cfec7
Rohan Barad <ronibara@umich.edu>
2019-12-24 13:53:24 -0500

    adding new html files for each page, as well as pictures

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-Hello World
+// Used to control the behavior of different elements
+// Ex: Animations, cool scrolling, dynamic
+
+document.getElementById('date').innerHTML = new Date().toDateString();
